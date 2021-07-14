5c22f1d462584a14471128f73d7345592e907bf9
sarahfalter <sarah.falter@ufl.edu>
2021-07-14 16:12:55 -0400

    Finish explore styling and add mapbox api to visit page

---
+++ map.js
@@ -0,0 +1,13 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2ZhbHRlcjIwMjEiLCJhIjoiY2tyM2x5anQ2MWoyMzJ4bGR0dHpxN2ZtYiJ9.V3SJC8WuShc1rkPtMyvTAQ';
+
+
+
+
+
+
+
+
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
