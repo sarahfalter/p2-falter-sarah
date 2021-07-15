337618b0d0086e1a08fae710cf816fb299df1ba9
sarahfalter <sarah.falter@ufl.edu>
2021-07-15 13:16:16 -0400

    Add mapbox api and finish troubleshooting

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-document.getElementById("dropbtn").addEventListener("click", function() {
-  var subMenu = document.getElementById("menuDropdown").style.display = "block";
+var subMenu = document.getElementById("menuDropdown");
+var isMouseOver = false;
+
+document.getElementById("dropbtn").addEventListener("mouseover", function() {
+  subMenu.style.display = "block";
 }
 );
 
-
-
+document.getElementById("menuDropdown").addEventListener("mouseout", function()
+{
+  subMenu.style.display = "none";
+}
+);
 
 
 document.getElementById("myBtn").addEventListener("click", function(){   
--- js/map.js
+++ js/map.js
@@ -1,13 +1,16 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2ZhbHRlcjIwMjEiLCJhIjoiY2tyM2x5anQ2MWoyMzJ4bGR0dHpxN2ZtYiJ9.V3SJC8WuShc1rkPtMyvTAQ';
 
-
-
-
-
-
-
-
 var map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11'
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-73.9632, 40.7794],
+zoom: 13
 });
+
+
+map.addControl(
+    new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+    }),
+    'top-left'
+    );
