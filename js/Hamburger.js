a8e3797afabdbc0051cfdc1ede4e0b4b92161b8c
sarahfalter <sarah.falter@ufl.edu>
2021-07-14 23:42:54 -0400

    Add hamburger menu and consolidate js

---
+++ js/Hamburger.js
@@ -0,0 +1,20 @@
+
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".mobile-menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".mobile-menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".mobile-menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+});
